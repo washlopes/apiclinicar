1a318bbb40123960f8dd4cfa02245dfd244cd51c
Washington Luís Pedrosa Lopes <washlopes@gmail.com>
2025-06-25 18:44:21 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/vectordev/apiclinicar/ApiclinicarApplication.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.apiclinicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiclinicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiclinicarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vectordev/apiclinicar/ApiclinicarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.apiclinicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiclinicarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
